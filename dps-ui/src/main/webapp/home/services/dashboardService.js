049d6b22319eb4568723010746e29102c605630e
Hardik <Hardik@Hardik-PC>
2016-08-20 19:31:01 +0530

    Services for Dashboard.

---
+++ dps-ui/src/main/webapp/home/services/dashboardService.js
@@ -0,0 +1,13 @@
+var dashboardServiceApp = angular.module('dashboardApp');
+
+dashboardServiceApp.factory('getDashboardCardService', function ($resource) {
+    return $resource('http://localhost:8080/dps-web-service-0.0.1/rest/dashboard');
+});
+
+dashboardServiceApp.factory('getDashboardConfigService', function ($resource) {
+    return $resource('http://localhost:8080/dps-web-service-0.0.1/rest/config');
+});
+
+dashboardServiceApp.factory('modifyDashboardConfigService', function ($resource) {
+    return $resource('http://localhost:8080/dps-web-service-0.0.1/rest/config/modify');
+});
