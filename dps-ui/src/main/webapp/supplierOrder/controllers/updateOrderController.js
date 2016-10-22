89dfd3f8c0d329c813f46d282fe38513d16e3ffc
Shankheshwar <Shankheshwar@Shankheshwar-PC>
2016-10-22 12:32:43 +0530

    Updated update order screen

--- dps-ui/src/main/webapp/supplierOrder/controllers/updateOrderController.js
+++ dps-ui/src/main/webapp/supplierOrder/controllers/updateOrderController.js
@@ -8,7 +8,9 @@ angular.module('updateOrderApp', [])
             $scope.sortType = 'productCode'; /* Set the default sort type */
             $scope.maskColumns = true; /* Hide the columns */
             $scope.isAccordionOpen = false; /* Collapse all the accordions */
-            $scope.editTableRow = {}; /* Object for inline editing in Order Summary table */            
+            $scope.editTableRow = {}; /* Object for inline editing in Order Summary table */           
+            $scope.accordionList = new Object(); /* List of Accordions */
+            $scope.selectAll = []; /* model for toggleAll as per list of accordions */
             $scope.accordianData = [ /* Object given by WS  */
                 {
                     "heading" : "Order 1",
@@ -71,11 +73,55 @@ angular.module('updateOrderApp', [])
             /* Function will be executed after the page is loaded */
             $scope.$on('$viewContentLoaded', function () {
             	
-            	/* Set variable for inline editing in order summary table */
-                for (var i = 0; i < $scope.accordianData[0].orderDetails.length; i++) {
-                    $scope.editTableRow[i] = false;
+            	/* Create array for toggleAll inside accordionList object */
+                for (var i = 0; i < $scope.accordianData.length; i++) {
+                	$scope.accordionList["selectedRows" + i] = [];
+                	$scope.selectAll[i] = false;
+                	
+                	/* Set variable for inline editing in update order table */
+                    for (var j = 0; j < $scope.accordianData[0].orderDetails.length; j++) {
+                        $scope.editTableRow[j] = false;
+                    }
                 }
             });   
+            
+            /* Function to select/unselect all the Orders from accordions */
+    	    $scope.toggleAll = function (index) {
+    	        if ($scope.selectAll[index]) {
+    	            $scope.selectAll[index] = true;
+    	            $scope.accordionList["selectedRows" + index] = [];
+    	            angular.forEach($scope.accordianData[index].orderDetails, function (order) {
+    	            	order.isChecked = $scope.selectAll[index]
+    	                $scope.accordionList["selectedRows" + index].push(1);
+    	            });
+    	        }
+    	        else {
+    	            $scope.selectAll[index] = false;
+    	            angular.forEach($scope.accordianData[index].orderDetails, function (order) {
+    	            	order.isChecked = $scope.selectAll[index]
+    	                $scope.accordionList["selectedRows" + index] = [];
+    	            });
+    	        }
+    	    };
+    	    
+    	    /* Function to select/unselect the Order from one accordion */
+    	    $scope.toggle = function (parentIndex, index, order) {
+    	        if (order.isChecked) {
+    	        	$scope.accordionList["selectedRows" + parentIndex].push(1);
+    	        }
+    	        else {
+    	        	$scope.accordionList["selectedRows" + parentIndex].pop();
+    	        }
+    	        if ($scope.accordianData[parentIndex].orderDetails.length === $scope.accordionList["selectedRows" + parentIndex].length) {
+    	            $scope.selectAll[parentIndex] = true;
+    	        }
+    	        else {
+    	        	$scope.selectAll[parentIndex] = false;
+    	        }
+    	        if ( $scope.accordionList["selectedRows" + parentIndex].length === 0) {
+    	        	$scope.selectAll[parentIndex] = false;
+    	        }
+    	    };
             
             /* Function to edit all the customer product margins */
             $scope.editAll = function () {
