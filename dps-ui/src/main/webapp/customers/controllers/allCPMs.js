2a30fca2c966f5fdb932b9b2140ba86ff5ea4846
Shankheshwar <Shankheshwar@Shankheshwar-PC>
2016-11-12 21:21:05 +0530

    Modified all CPMs screen

--- dps-ui/src/main/webapp/customers/controllers/allCPMs.js
+++ dps-ui/src/main/webapp/customers/controllers/allCPMs.js
@@ -1,26 +1,18 @@
 angular.module('allCPMsApp', ['angularUtils.directives.dirPagination', 'smoothScroll'])
         .controller('allCPMsController', function ($scope, $rootScope, $timeout, getCustomersProductPreferencesService,
         		getProductPreferencesService, getProductsService, smoothScroll) {
-            /* Initialize the page variables */
             $scope.showErrorBox = false; /* Hide the Error Box */
-            $scope.errorMessage = "";
             $scope.searchedResults = false; /* Hide the search results container */
             $scope.sortOrder = false; /* Set the default sort order */
             $scope.sortType = 'productCode'; /* Set the default sort type */
-            $scope.maskColumns = true; /* Hide the columns */
             $scope.products = [];
-            $scope.customers; /* Object for storing customers list */
 
             /* Function will be executed after the page is loaded */
-            $scope.$on('$viewContentLoaded', function () {
-            	  
-            	
-            });
+            $scope.$on('$viewContentLoaded', function () {});
             
             /* Function to search for Products */
             $scope.searchProduct = function () {    
             	angular.element(document.querySelector('.loader')).addClass('show');
-                /* Service Call to retrieve searched product */
             	$scope.products = getProductsService.query({code:$scope.searchProductCode}, function(){/* Success Callback */
     	        	$timeout(function(){
     	        		$scope.searchedResults = true;
@@ -32,7 +24,6 @@ angular.module('allCPMsApp', ['angularUtils.directives.dirPagination', 'smoothSc
     	        	$timeout(function(){
     	        		$scope.searchedResults = false;
     	                $scope.showErrorBox = true;
-    	        		$scope.errorMessage = "CPMs could not be retrieved. Please try again after some time";
     	        		angular.element(document.querySelector('.loader')).removeClass('show');
     	        		console.log($scope.customers)
     	        	}, 500);
