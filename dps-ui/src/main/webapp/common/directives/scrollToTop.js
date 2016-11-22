88cb975955f1fae0a0ee669642349496729e3d9d
Shankheshwar <Shankheshwar@Shankheshwar-PC>
2016-11-22 08:51:23 +0530

    scrollToTop difective modified

--- dps-ui/src/main/webapp/common/directives/scrollToTop.js
+++ dps-ui/src/main/webapp/common/directives/scrollToTop.js
@@ -5,11 +5,11 @@ var DPSApp = angular.module('DPSApp');
 DPSApp.directive('scrollToTop', function ($window) {
 	return{
 		restrict: "A",
-		template: "<md-button class='md-fab md-mini md-primary scrollToTop' title='Scroll to top' aria-label='Scroll to top' scroll-to='DPSApp' ng-show='scrollToTop'></md-button>",
+		template: "<md-button class='md-fab md-mini md-primary md-hue-3 scrollToTop' title='Scroll to top' aria-label='Scroll to top' scroll-to='DPSApp' ng-show='scrollToTop'></md-button>",
 		link: function(scope, element){
 			scope.scrollToTop = false; /* Hide the scroll to top icon */ 
 			angular.element($window).bind("scroll", function(){
-				if (this.pageYOffset >= 200) {
+				if (this.pageYOffset >= 180) {
 					scope.scrollToTop = true;
 	             } else {
 	            	 scope.scrollToTop = false;
