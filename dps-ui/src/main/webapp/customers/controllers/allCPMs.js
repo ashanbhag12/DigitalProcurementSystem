f585c808f113f608327d84bdd34b3e424c065a94
Shankheshwar <Shankheshwar@Shankheshwar-PC>
2016-11-27 16:02:00 +0530

    Added All CPM screen

--- dps-ui/src/main/webapp/customers/controllers/allCPMs.js
+++ dps-ui/src/main/webapp/customers/controllers/allCPMs.js
@@ -1,38 +1,44 @@
 angular.module('allCPMsApp', ['angularUtils.directives.dirPagination', 'smoothScroll'])
-        .controller('allCPMsController', function ($scope, $rootScope, $timeout, getCustomersProductPreferencesService,
-        		getProductPreferencesService, getProductsService, smoothScroll) {
+        .controller('allCPMsController', function ($scope, $rootScope, $timeout, getAllCPM, smoothScroll) {
             $scope.showErrorBox = false; /* Hide the Error Box */
             $scope.searchedResults = false; /* Hide the search results container */
             $scope.sortOrder = false; /* Set the default sort order */
-            $scope.sortType = 'productCode'; /* Set the default sort type */
-            $scope.products = [];
+            $scope.sortType = 'shipmark'; /* Set the default sort type */
+            $scope.allCPMs = []; /* Object for all CPMs of searched product */
+            var scrollOptions = { /* Set offset to scroll to search table */
+            	    offset: -175,
+            	};
 
             /* Function will be executed after the page is loaded */
             $scope.$on('$viewContentLoaded', function () {});
             
-            /* Function to search for Products */
-            $scope.searchProduct = function () {    
-            	angular.element(document.querySelector('.loader')).addClass('show');
-            	$scope.products = getProductsService.query({code:$scope.searchProductCode}, function(){/* Success Callback */
-    	        	$timeout(function(){
-    	        		$scope.searchedResults = true;
-    	                $scope.showErrorBox = false;
-    	        		angular.element(document.querySelector('.loader')).removeClass('show');
-    	        	}, 500);
-    	        }, function(){ /* Error Callback */
-    	        	$timeout(function(){
-    	        		$scope.searchedResults = false;
-    	                $scope.showErrorBox = true;
-    	        		angular.element(document.querySelector('.loader')).removeClass('show');
-    	        	}, 500);
-    	        });
+            /* Function to get all CPMs for searched product */
+            $scope.getProductCPM = function () {   
+            	if($scope.searchProductCode !== undefined && $scope.searchProductCode !== ""){
+            		angular.element(document.querySelector('.loader')).addClass('show');
+                	$scope.allCPMs = getAllCPM.query({productCode:$scope.searchProductCode}, function(){/* Success Callback */
+        	        	$timeout(function(){
+        	        		$scope.searchedResults = true;
+        	                $scope.showErrorBox = false;
+        	        		angular.element(document.querySelector('.loader')).removeClass('show');
+        	        		console.log($scope.allCPMs);
+        	        		smoothScroll(document.getElementsByClassName("searchedResults"), scrollOptions); /* Scroll to the table */
+        	        	}, 500);
+        	        }, function(){ /* Error Callback */
+        	        	$timeout(function(){
+        	        		$scope.searchedResults = false;
+        	                $scope.showErrorBox = true;
+        	        		angular.element(document.querySelector('.loader')).removeClass('show');
+        	        	}, 500);
+        	        });
+            	}            	
             };
             
-            /* Trigger searchProduct() function on "enter" key press in search product field */
+            /* Trigger getProductCPM() function on "enter" key press in search product field */
             $scope.triggerSearchProduct = function(event){
             	if(event.which === 13) {
                     event.preventDefault();
-                    $scope.searchProduct();
+                    $scope.getProductCPM();
                 }
             };
         });
---
+++ dps-ui/src/main/webapp/customers/services/allCPMService.js
@@ -0,0 +1,5 @@
+angular.module('allCPMsApp')
+	
+	.factory('getAllCPM', function ($resource) {
+	    return $resource('http://localhost:8080/dps-web-service-0.0.1/rest/allcpm/:productCode', {productCode:"@productCode"});
+	});
